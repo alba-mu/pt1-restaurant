ad3534588326a23b66aeafecde144be1a1e99a71
alba-mu <albamu2002@gmail.com>
2025-11-07 18:08:37 +0100

    Implement logout page

--- logout.php
+++ logout.php
@@ -2,8 +2,32 @@
 session_start();
 if (isset($_SESSION['role'])) {  //session exists
     session_destroy();
-    header("Location: index.php");    
+    $loggedOut = True;    
 } else {
-    //TODO: decide how to treat this case.
     header("Location: login.php");    
 }
+?>
+<?php if ($loggedOut) : ?>
+    <!DOCTYPE html>
+    <html lang="ca">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Tancament de sessió</title>
+
+        <!-- Bootstrap 5 -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
+
+    <body class="bg-dark text-light d-flex justify-content-center align-items-center vh-100">
+            <div class="card text-center p-4 shadow-lg" style="max-width: 400px;">
+                <div class="card-body">
+                    <h2 class="card-title mb-3">Has tancat sessió correctament</h2>
+                    <p class="card-text mb-4">Fins aviat!</p>
+                    <a href="index.php" class="btn btn-dark text-white w-100">Tornar a la pàgina principal</a>
+                </div>
+            </div>
+    </body>
+    </html>
+<?php endif; ?>
