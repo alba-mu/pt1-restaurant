edbabed0d4c029871bebd72082feb1383949ec27
alba-mu <albamu2002@gmail.com>
2025-11-09 17:23:00 +0100

    Format code

--- logout.php
+++ logout.php
@@ -2,14 +2,15 @@
 session_start();
 if (isset($_SESSION['role'])) {  //session exists
     session_destroy();
-    $loggedOut = True;    
+    $loggedOut = True;
 } else {
-    header("Location: login.php");    
+    header("Location: login.php");
 }
 ?>
-<?php if ($loggedOut) : ?>
+<?php if ($loggedOut): ?>
     <!DOCTYPE html>
     <html lang="ca">
+
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -21,13 +22,14 @@ if (isset($_SESSION['role'])) {  //session exists
     </head>
 
     <body class="bg-dark text-light d-flex justify-content-center align-items-center vh-100">
-            <div class="card text-center p-4 shadow-lg" style="max-width: 400px;">
-                <div class="card-body">
-                    <h2 class="card-title mb-3">Has tancat sessió correctament</h2>
-                    <p class="card-text mb-4">Fins aviat!</p>
-                    <a href="index.php" class="btn btn-dark text-white w-100">Tornar a la pàgina principal</a>
-                </div>
+        <div class="card text-center p-4 shadow-lg" style="max-width: 400px;">
+            <div class="card-body">
+                <h2 class="card-title mb-3">Has tancat sessió correctament</h2>
+                <p class="card-text mb-4">Fins aviat!</p>
+                <a href="index.php" class="btn btn-dark text-white w-100">Tornar a la pàgina principal</a>
             </div>
+        </div>
     </body>
+
     </html>
 <?php endif; ?>
