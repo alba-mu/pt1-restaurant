0e32e69a6a7a8149007ffcba1cfa4d32de21ce07
alba-mu <albamu2002@gmail.com>
2025-11-14 15:18:41 +0100

    Implement daymenu main content

--- daymenu.php
+++ daymenu.php
@@ -1,21 +1,39 @@
 <?php
 session_start();
 $current_page = 'daymenu.php';
+
+$dayMenu_filepath = 'files/daymenu.txt';
+$categories_filepath = 'files/categories.txt';
+
+include_once 'fn-php/fn-menu.php';
+
+// Generar lllistes per categoria
+$lists = generateDayMenuLists($dayMenu_filepath, $categories_filepath);
 ?>
 
 <?php include_once "topmenu.php"; ?>
 
-<main class="flex-grow-1 container py-4">
+<main class="flex-grow-1 container-fluid py-4">
     <div class="container-fluid">
-        <div class="container">
-            <h2>Day menu</h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum
-            </p>
+        <div class="container border border-2 border-muted rounded-3">
+            <h1 class="text-center display-3 mt-0 bg-dark-subtle pb-2 mb-3">Menú del Dia</h1>
+            <div class="row g-3">
+
+                <?php
+                    if (isset($lists['error'])) {
+                        echo "<div class='alert alert-danger'>{$lists['error']}</div>";
+                    } else {
+                        foreach ($lists as $list) {
+                            echo "<div class='col-12 d-flex justify-content-center'>
+                                    <div class='w-100 text-center' style='max-width: 650px;'>
+                                        $list
+                                    </div>
+                                  </div>";
+                        }
+                    }
+                ?>
+
+            </div>
         </div>
     </div>
 </main>
